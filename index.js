aa960355e4db476d1d7ed4d763dfe1e710c2c966
Stiven Parra <michaelparraforero236@gmail.com>
2024-03-05 22:27:43 -0500

    array con diferentes elementos

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
 let miArray = []; // Array vacío
 
 let miArray2 = [1, 2, 3, 4]; // Array con elementos segunda rama
+
+let miArray3 = [1, "Stiven", false, {nombre: "anyi"}, ["verde", "verde", "verde"]];
