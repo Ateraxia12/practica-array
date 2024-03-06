845ba96388be04a3837d047f52a2062ccb2d6bc4
Stiven Parra <michaelparraforero236@gmail.com>
2024-03-05 22:37:00 -0500

    array constructor

--- index.js
+++ index.js
@@ -3,3 +3,5 @@ let miArray = []; // Array vacío
 let miArray2 = [1, 2, 3, 4]; // Array con elementos segunda rama
 
 let miArray3 = [1, "Stiven", false, {nombre: "anyi"}, ["verde", "verde", "verde"]];
+
+let miArray4 = new Array(10, 27, 31, 43, 56);//array constructor
