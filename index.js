8adfaba571131b008c693c7f9d981afd356c8049
Stiven Parra <michaelparraforero236@gmail.com>
2024-03-05 22:49:03 -0500

    array de secuencia

--- index.js
+++ index.js
@@ -5,3 +5,5 @@ let miArray2 = [1, 2, 3, 4]; // Array con elementos segunda rama
 let miArray3 = [1, "Stiven", false, {nombre: "anyi"}, ["verde", "verde", "verde"]];
 
 let miArray4 = new Array(10, 27, 31, 43, 56);//array constructor
+
+let arraySecuencia = Array.from({ length: 15 }, (_, index) => index + 1);//array secuencia numérica
