e27a568e0ec63ac3a82f4df63f62bb81012ea29b
Stiven Parra <michaelparraforero236@gmail.com>
2024-03-05 22:54:06 -0500

    array de repeticion

--- index.js
+++ index.js
@@ -7,3 +7,5 @@ let miArray3 = [1, "Stiven", false, {nombre: "anyi"}, ["verde", "verde", "verde
 let miArray4 = new Array(10, 27, 31, 43, 56);//array constructor
 
 let arraySecuencia = Array.from({ length: 15 }, (_, index) => index + 1);//array secuencia numérica
+
+let array5 = new Array(5).fill(0);//array de repeticion
